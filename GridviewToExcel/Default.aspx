1868e37f944e86af094c3d78012c712929ab17a7
dsuraj <deshpande.suraj@gmail.com>
2013-09-01 20:18:15 +0530

    Initial commit

---
+++ GridviewToExcel/Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="GridviewToExcel.Default" EnableEventValidation="false" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="EmployeeID" DataSourceID="SqlDataSource1" AllowPaging="True" AllowSorting="True">
+                <Columns>
+                    <asp:BoundField DataField="EmployeeID" HeaderText="EmployeeID" ReadOnly="True" InsertVisible="False" SortExpression="EmployeeID"></asp:BoundField>
+                    <asp:BoundField DataField="FirstName" HeaderText="FirstName" SortExpression="FirstName"></asp:BoundField>
+                    <asp:BoundField DataField="LastName" HeaderText="LastName" SortExpression="LastName"></asp:BoundField>
+                    <asp:BoundField DataField="Address" HeaderText="Address" SortExpression="Address"></asp:BoundField>
+                    <asp:BoundField DataField="City" HeaderText="City" SortExpression="City"></asp:BoundField>
+                    <asp:BoundField DataField="PostalCode" HeaderText="PostalCode" SortExpression="PostalCode"></asp:BoundField>
+                    <asp:BoundField DataField="Country" HeaderText="Country" SortExpression="Country"></asp:BoundField>
+                </Columns>
+            </asp:GridView>
+            <asp:SqlDataSource runat="server" ID="SqlDataSource1" ConnectionString='<%$ ConnectionStrings:NorthwindConnectionString %>' SelectCommand="SELECT [EmployeeID], [FirstName], [LastName], [Address], [City], [PostalCode], [Country] FROM [Employees]"></asp:SqlDataSource>
+            <asp:Button ID="Button1" runat="server" Text="Export to Excel" OnClick="Button1_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ GridviewToExcel/Default.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GridviewToExcel
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.ClearContent();
+            Response.Buffer = true;
+            Response.AddHeader("content-disposition", string.Format("attachment; filename={0}", "EmployeesData.xls"));
+            Response.ContentType = "application/ms-excel";
+
+            StringWriter stringWriter = new StringWriter();
+            HtmlTextWriter htmlTextWriter = new HtmlTextWriter(stringWriter);
+
+            GridView1.AllowPaging = false;
+            GridView1.DataBind();
+
+            //This will change the header background color
+            GridView1.HeaderRow.Style.Add("background-color", "#FFFFFF");
+
+            //This will apply style to gridview header cells
+            for (int index = 0; index < GridView1.HeaderRow.Cells.Count; index++)
+            {
+                GridView1.HeaderRow.Cells[index].Style.Add("background-color", "#d17250");
+            }
+
+            int index2 = 1;
+            //This will apply style to alternate rows
+            foreach (GridViewRow gridViewRow in GridView1.Rows)
+            {
+                gridViewRow.BackColor = Color.White;
+                if (index2 <= GridView1.Rows.Count)
+                {
+                    if (index2 % 2 != 0)
+                    {
+                        for (int index3 = 0; index3 < gridViewRow.Cells.Count; index3++)
+                        {
+                            gridViewRow.Cells[index3].Style.Add("background-color", "#eed0bb");
+                        }
+                    }
+                }
+                index2++;
+            }
+
+            GridView1.RenderControl(htmlTextWriter);
+
+            Response.Write(stringWriter.ToString());
+            Response.End();
+        }
+
+        public override void VerifyRenderingInServerForm(Control control)
+        {
+            /* Verifies that the control is rendered */
+        }
+    }
+}
